1301e3e4af0d48b6c515a8ef8cad703b1886d51b
Rugved Wagh <89897353+Rugved76@users.noreply.github.com>
2024-07-17 00:11:54 +0530

    Delete models directory

--- models/todo.model.js
+++
@@ -1,9 +0,0 @@
-// backend/models/todo.model.js
-const mongoose = require('mongoose');
-
-const todoSchema = new mongoose.Schema({
-    text: { type: String, required: true },
-    isCompleted: { type: Boolean, default: false },
-});
-
-module.exports = mongoose.model('Todo', todoSchema);
